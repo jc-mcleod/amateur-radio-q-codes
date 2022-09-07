4b0fb43bc1d20783567cd7a7f5f4a33bb2bc2c7a
James <contact@jcmcleod.com>
2022-09-07 19:49:15 +0100

    added 'checkAnswer' function.

--- q_codes_tester.c
+++ q_codes_tester.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int checkAnswer(char* qCode, char* answer)
+{
+	int i = 0;
+	char chr;
+	char upperAnswer[4];
+
+    while (answer[i]) { 
+        chr = answer[i]; 
+		chr = toupper(chr);	
+		strncat(upperAnswer, &chr, 1);
+        i++; 
+    } 
+
+	if (strcmp(qCode, upperAnswer) == 0)
+	{
+		return 0;
+		
+	}
+	else
+	{
+		return 1;
+	}
+}
 
 void main()
 {
@@ -10,9 +36,21 @@ void main()
 	int i;
 	int numQuestions = 10;
 	char answer[4];	
+	char points;	
+	int dummy;
 
+	system("clear");
+	
 	if (fp != NULL)
 	{
+		printf("How many questions would you like? (max 42)\n\n"); 
+		scanf("%d", &numQuestions);
+
+		//clear input buffer
+		while ((dummy = getchar()) != '\n' && dummy != EOF) { }	
+
+		system("clear");
+
 		for (i = 0; i < numQuestions; i++)
 		{
 			char line[100];
@@ -21,32 +59,40 @@ void main()
 			//splits qcodes and questions and saves them into separate variables
 			question = strtok(line, ",");
 			qCode = strtok(NULL, ",");
-			printf("Question = %s\n\n", question);
-			scanf("%3s", answer);
-			printf("%s", answer);
+
+			//removes newline
+			qCode[strcspn(qCode, "\n")] = 0;
+
+			printf("[Question %d] What is the code for: '%s'\n\n", i + 1, question);
+			scanf(" %3s", answer);
 	
 			//clear input buffer
-			int dummy;
 			while ((dummy = getchar()) != '\n' && dummy != EOF) { }	
 			
-			if (checkAnswer() == 0)
+			if (checkAnswer(qCode, answer) == 0)
 			{
-				printf("Correct! The answer was %s.", answer);		
+				system("clear");
+				printf("Correct! The answer was: %s.\n\n", qCode);		
+				points++;
+				sleep(2);
+			}
+			else 
 			{
-		}	
-	
-	fclose(fp);
+				system("clear");
+				printf("WRONG. The correct answer was %s.\n\n", qCode);
+				sleep(2);
+			}
+			system("clear");
+		}		
+		fclose(fp);	
 	}
 	else
 	{
 		perror("q_codes_list.csv");
 	}
-}
-
-int checkAnswer(char* qCode, char* answer)
-{
-
+	
+	printf("In total, you scored %d/%d.\n\n", points, numQuestions);
 }
 
 
-//TODO: IMPLEMENT RANDOMNESS, COMPARE QUESTION WITH ANSWER, POINTS SYSTEM, ASK HOW MANY QUESTIONS FIRST
+//TODO: IMPLEMENT RANDOMNESS
